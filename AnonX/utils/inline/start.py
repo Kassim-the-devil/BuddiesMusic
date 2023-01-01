15645bfab43ce10e1ae88b52af24c2d23a2cf594
Kassim darlzzz <117517237+Kassim-the-devil@users.noreply.github.com>
2023-01-01 17:56:52 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -44,7 +44,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
+                text="《 ʜᴇʟᴩ 》", callback_data="settings_back_helper"
             ),
         ],
         [
